7e349c8d8804cda60730dbfa6efa9d010501ee08
Moses Williams III <varenigmatictheprogrammer1@gmail.com>
2016-04-20 17:18:54 -0400

    C#
    Example in C# for the .Net framework by Microsoft. Enjoy and please add and or correct!

---
+++ c_sharp.cs
@@ -0,0 +1,24 @@
+//By Moses Williams III | varEnigmatic
+/*
+This is a display of structure in C# programming.  I hope this helps a little!
+Please add to it as we can help others develop and grow.  Thank you
+*/
+
+namespace whatYouLikeNamespace
+{
+    class GiveANameWithMeaningClass { }
+
+    interface HelloInterface { }
+
+    struct WhatAStruct { }
+
+    enum HelloEnum { a, b }
+
+    delegate void NameWithMeaningDelegate(int i);
+    //Don't forget to comment in your code! :)
+
+    namespace HereWeGoNamespace.Nested
+    {
+        class AnotherNameingWithMeaningClass2 { }
+    }
+}
